2a1a26136e9d69fa8c87bd8b483c4090b59bb490
Jon Jagger <jon@jaggersoft.com>
2020-01-18 20:05:35 +0000

    Switch to circleci

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -1,6 +1,22 @@
-#!/bin/bash
-set -e
+#!/bin/bash -Eeu
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly TMP_DIR="$(mktemp -d /tmp/cyber-dojo-languages.XXXXXXX)"
+remove_tmp_dir() { rm -rf "${TMP_DIR}" > /dev/null; }
+trap remove_tmp_dir INT EXIT
 
-${MY_DIR}/../image_builder/run_build_image.sh
+script()
+{
+  local -r name=build_test_push_notify.sh
+  local -r local_path="${MY_DIR}/../image_builder/${name}"
+  local -r remote_path="https://raw.githubusercontent.com/cyber-dojo-languages/image_builder/master/${name}"
+  if [ -f "${local_path}" ]; then
+    echo "${local_path}"
+  else
+    curl --fail --output "${TMP_DIR}/${name}" --silent "${remote_path}"
+    chmod 700 "${TMP_DIR}/${name}"
+    echo "${TMP_DIR}/${name}"
+  fi
+}
+
+"$(script)" "${MY_DIR}"
